d6cb954aeb534d2a1075a6bc68dcfe66108c9b09
james <caiqiyu@live.cn>
2021-04-20 08:23:48 +0800

    start

---
+++ src/Parser.java
@@ -0,0 +1,9 @@
+import java.io.FileInputStream;
+import java.util.Scanner;
+
+public class Parser {
+    public Parser(FileInputStream fileInputStream) {
+        Scanner scanner = new Scanner(fileInputStream);
+    }
+
+}
